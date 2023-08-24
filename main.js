931495f84672c1fcb2f1e9b920d3ebb225f08219
Mahadev k kawane <101266874+mahadevkawane@users.noreply.github.com>
2023-08-24 09:43:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+// console.log("Hello world");
+// let name="mahadev"
+// console.log(name);
+// let num1=22;
+// let num2=20;
+// let sum=num1+num2;
+// console.log(sum)
+// alert("Your name is " +name+ " sum is " +sum)
+
+// let textbox
+// document.getElementById("btn").onclick=function()
+// {
+//     console.log("Button is clicked")
+//     textbox = document.getElementById("txt").value 
+//     console.log(textbox)
+//     // document.getElementById("headline").innerHTML=sum
+//     document.getElementById("headline").value
+// }
+
+let textbox
+let mail
+let password
+document.getElementById("btn").onclick=function()
+{
+    console.log("Submitted")
+    textbox = document.getElementById("txt").value 
+    console.log(textbox)
+    // document.getElementById("headline").innerHTML=sum
+    document.getElementById("headline").value
+    document,getElementById("mail").value
+    console.log(mail)
+    document.getElementById("pass").value
+    console.log(password)
+}
